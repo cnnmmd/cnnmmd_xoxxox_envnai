f0e0ad09fd1f987ad236bbd0d7f1ee40974f32ff
azsgit <10238943+azsgit@users.noreply.github.com>
2025-07-27 22:05:20 +0900

    migrate from local repo

---
+++ manage/bin/create.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pthtop="$(cd "$(dirname "${0}")/../../../.." && pwd)"
+source "${pthtop}"/manage/lib/params.sh
+source "${pthtop}"/manage/lib/shared.sh
+source "${pthcrr}"/params.sh
+
+pthkey="${HOME}"/sec/key/.envath_nai.txt
+pthlib="${pthsrc}"/export/lib/xoxxox/naiapi
+
+if test ! -e "${pthlib}"
+then
+  if cnfrtn "import: boilerplate.py: https://github.com/Aedial/novelai-api/blob/main/example/boilerplate.py"
+  then
+    mkdir -p "${pthlib}"
+    cd "${pthlib}"
+    curl -O https://raw.githubusercontent.com/Aedial/novelai-api/main/example/boilerplate.py
+    "${pthtop}"/manage/bin/remove.sh
+    "${pthtop}"/manage/bin/append.sh
+  fi
+fi
+
+if test ! -e "${pthkey}"
+then
+  cnfrtn "manual: ${pthkey}: NAI_USERNAME=XXX / NAI_PASSWORD=XXX"
+fi
+addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
---
+++ manage/bin/params.sh
@@ -0,0 +1,3 @@
+cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envnai.txt
+pthdoc="${pthtop}"/export/cnf/xoxxox/docker
+imgtgt='xoxxox_envnai:001'
